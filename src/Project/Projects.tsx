d79e6f70d4f5db98e2211ec267a763de94da43aa
tmagar <tmagar448@gmail.com>
2023-02-20 19:23:28 -0500

    designing and removing some codes to make the codes looks clean

--- src/Project/Projects.tsx
+++ src/Project/Projects.tsx
@@ -1,67 +1,70 @@
+import { Container, Row, Col, Card } from "react-bootstrap";
 import { useInView } from "react-intersection-observer";
 import { Link } from "react-router-dom";
 
-function ProjectCard({
-  imgSrc,
-  imgAlt,
-  title,
-  text,
-  animationClass,
-}: {
-  imgSrc: string;
-  imgAlt: string;
-  title: string;
-  text: string;
-  animationClass: string;
-}) {
-  const { ref, inView } = useInView({ threshold: 0.5 });
-
-  return (
-    <div className="col-md-4 mb-4">
-      <div
-        ref={ref}
-        className={inView ? `card ${animationClass} in-view` : animationClass}
-      >
-        <img className="card-img-top" src={imgSrc} alt={imgAlt} />
-        <div className="card-body">
-          <h5 className="card-title">{title}</h5>
-          <p className="card-text">{text}</p>
-          <Link to="#" className="btn btn-primary">
-            View details
-          </Link>
-        </div>
-      </div>
-    </div>
-  );
-}
-
 function Projects() {
+  const { ref, inView } = useInView({ threshold: 0.5 });
+  const ProjectData = [
+    {
+      id: 1,
+      imgSrc: "https://via.placeholder.com/300x200.png?text=Project+1",
+      imgAlt: "Project 1",
+      title: "Project 1",
+      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      animationClass: "animate-left",
+    },
+    {
+      id: 2,
+      imgSrc: "https://via.placeholder.com/300x200.png?text=Project+2",
+      imgAlt: "Project 2",
+      title: "Project 2",
+      text: "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+      animationClass: "animate-right",
+    },
+    {
+      id: 3,
+      imgSrc: "https://via.placeholder.com/300x200.png?text=Project+3",
+      imgAlt: "Project 3",
+      title: "Project 3",
+      text: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.",
+      animationClass: "animate-bottom",
+    },
+  ];
+
   return (
-    <div className="container py-5 ">
-      <div className="row">
-        <ProjectCard
-          imgSrc="https://via.placeholder.com/300x200.png?text=Project+1"
-          imgAlt="Project 1"
-          title="Project 1"
-          text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-          animationClass="animate-left"
-        />
-        <ProjectCard
-          imgSrc="https://via.placeholder.com/300x200.png?text=Project+2"
-          imgAlt="Project 2"
-          title="Project 2"
-          text="Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-          animationClass="animate-right"
-        />
-        <ProjectCard
-          imgSrc="https://via.placeholder.com/300x200.png?text=Project+3"
-          imgAlt="Project 3"
-          title="Project 3"
-          text="Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur."
-          animationClass="animate-bottom"
-        />
-      </div>
-    </div>
+    <Container className="my-5 py-5">
+      <Row className="mb-5 pb-5">
+        {ProjectData.map((project) => (
+          <Col md={4} className="mb-4" key={project.id}>
+            <Card
+              ref={ref}
+              className={
+                inView
+                  ? `projectCard ${project.animationClass} in-view h-100`
+                  : project.animationClass
+              }
+            >
+              <Card.Img
+                variant="top"
+                src={project.imgSrc}
+                alt={project.imgAlt}
+              />
+              <Card.Body className="d-flex flex-column">
+                <Card.Title>{project.title}</Card.Title>
+                <Card.Text>{project.text}</Card.Text>
+                <Link
+                  to="#"
+                  className="mt-auto mx-auto btn btn-primary"
+                  style={{ width: "150px" }}
+                >
+                  View details
+                </Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
 
