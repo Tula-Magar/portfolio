65bd86188dd43c8d3cbc9df25fd6de5bc1a54e7a
TulaMagar <tmagar448@gmail.com>
2023-03-14 11:07:40 -0400

    changing background color

--- src/General/Home.tsx
+++ src/General/Home.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Col, Container, Row } from "react-bootstrap";
+import { Col, Container, Row, Image } from "react-bootstrap";
 import Projects from "../Project/Projects";
 import Skills from "./Skills";
 
@@ -8,82 +8,31 @@ import ChatBox from "./ChatBox";
 export default function Home() {
   return (
     <>
-      <Container className="my-5 mt-3 py-5 text-dark ">
-        <Container fluid className=" my-5 py-5 aboutmeContainer">
-          <Row>
-            <Col>
-              <Card className="p-4 FirstAboutMe">
-                <Card.Body>
-                  <Card.Title className="text-center aboutme">
-                    Hi, my name is Tula Magar, and I am an experienced ASP.NET
-                    Core full stack developer.
-                  </Card.Title>
-                  <Card.Text className="text-center aboutme">
-                    I have a passion for developing modern, scalable, and secure
-                    web applications. With <strong>5 years</strong> of
-                    experience, I have a proven track record of delivering
-                    high-quality projects for a variety of clients and
-                    industries.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-
-          <Row className="my-5 SecondAboueMe">
-            <Col md={6}>
-              <Card className="p-4 h-100 SecondAboutMe">
-                <Card.Body>
-                  <Card.Title className="aboutme">
-                    Full Stack Development
-                  </Card.Title>
-                  <Card.Text className="aboutme">
-                    As a full stack developer, I have a solid understanding of
-                    both front-end and back-end development. I have extensive
-                    experience in using ASP.NET Core to build robust and
-                    scalable APIs, and I am proficient in using technologies
-                    such as HTML, CSS, JavaScript, and React.js to create
-                    engaging user interfaces.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-            <Col md={6}>
-              <Card className="p-4 h-100">
-                <Card.Body>
-                  <Card.Title className="aboutme">
-                    Proactive & Collaborative
-                  </Card.Title>
-                  <Card.Text className="aboutme">
-                    In addition to my technical skills, I am also a proactive
-                    and collaborative team player. I enjoy working with others
-                    to find creative solutions to complex problems, and I am
-                    committed to delivering projects on time and within budget.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-
-          <Row className="mb-5 pb-5">
-            <Col>
-              <Card className="p-4 ThirdAboueMe">
-                <Card.Body>
-                  <Card.Title className="text-center aboutme">
-                    Ready for Your Project
-                  </Card.Title>
-                  <Card.Text className="text-center aboutme">
-                    If you are looking for a talented and dedicated ASP.NET Core
-                    full stack developer, I would be an excellent fit for your
-                    project. I am eager to put my skills and experience to work
-                    for you, and I look forward to the opportunity to discuss
-                    your requirements in more detail.
-                  </Card.Text>
-                </Card.Body>
-              </Card>
-            </Col>
-          </Row>
-        </Container>
+      <Container className="vh-100 w-100 d-flex justify-content-center align-items-center">
+        <Row>
+          <Col md={4} className="text-center">
+            <Image
+              src="https://via.placeholder.com/150"
+              alt="Profile Picture"
+              className="rounded-circle mb-3"
+              fluid
+            />
+            <h2>John Doe</h2>
+            <p>Web Developer</p>
+            <a href="/TulaMagarResume.pdf" className="btn btn-info" download>
+              Download
+            </a>
+          </Col>
+          <Col md={8}>
+            <h2>About Me</h2>
+            <p>
+              As a Full Stack developer, I have experience and knowledge in the
+              front-end, back-end and database aspects of web development. I am
+              proficient in a wide range of programming languages, frameworks,
+              and technologies, including but not limited to:
+            </p>
+          </Col>
+        </Row>
       </Container>
 
       <Projects />
