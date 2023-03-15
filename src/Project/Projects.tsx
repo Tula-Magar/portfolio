64cafc1229c8525583e8414c2f38af58e93a2309
TulaMagar <tmagar448@gmail.com>
2023-03-15 12:28:34 -0400

    updating projects page and it's style

--- src/Project/Projects.tsx
+++ src/Project/Projects.tsx
@@ -1,17 +1,36 @@
+import { useState } from "react";
 import { Container, Row, Col, Card } from "react-bootstrap";
 
 import { Link } from "react-router-dom";
 
+interface Project {
+  id: number;
+  imgSrc: string;
+  imgAlt: string;
+  title: string;
+  projectLink: string;
+  ProjectLive: string;
+  text: string | string[];
+  animationClass: string;
+}
+
 function Projects() {
-  const ProjectData = [
+  const [searchInput, setSearchInput] = useState("");
+  const ProjectData: Project[] = [
     {
       id: 1,
-      imgSrc: "https://via.placeholder.com/300x200.png?text=Project+1",
+      imgSrc: "https://via.placeholder.com/320x250.png?text=Project+2",
       imgAlt: "Project 1",
       title: "Online Soccer Shopping",
       projectLink: "https://github.com/Tula-Magar/OnlineSoccerShopping",
       ProjectLive: "https://onlinesoccershopping.herokuapp.com/",
-      text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+      text: [
+        "This project is a web application built with ASP.NET Core and ReactJS, using Bootstrap and React-Bootstrap for styling. The application allows users to browse products, add them to a shopping cart, and place orders.",
+        "The backend is built with ASP.NET Core, providing RESTful APIs to interact with the database and process orders.",
+        "The frontend is built with ReactJS, providing a responsive and dynamic user interface for browsing and ordering products.",
+        "Bootstrap and React-Bootstrap are used to provide a consistent and modern styling throughout the application, making it easy to use and visually appealing.",
+        "This project showcases the use of ASP.NET Core and ReactJS to build a full-stack web application, integrating different technologies and libraries to provide a complete user experience.",
+      ],
       animationClass: "animate-left",
     },
     {
@@ -36,47 +55,115 @@ function Projects() {
     },
   ];
 
+  const filteredProjects = ProjectData.filter((project) =>
+    project.title.toLowerCase().includes(searchInput?.toLowerCase())
+  );
+
   return (
     <Container className="my-5 py-5">
       <h2 className="text-center mb-5">Projects</h2>
-      <Row className="mb-5 pb-5">
-        {ProjectData.map((project) => (
-          <Col md={4} className="mb-4" key={project.id}>
-            <Card className="in-view h-100">
-              <Card.Img
-                variant="top"
-                src={project.imgSrc}
-                alt={project.imgAlt}
-              />
-              <Card.Body className="d-flex flex-column">
-                <Card.Title>{project.title}</Card.Title>
-                <Card.Text>{project.text}</Card.Text>
+      <div className="mb-4">
+        <input
+          type="text"
+          placeholder="Search projects"
+          value={searchInput}
+          onChange={(e) => setSearchInput(e.target.value)}
+        />
+      </div>
+      {filteredProjects.map((project) => (
+        <Row md={12} className="mb-5 pb-5" key={project.id}>
+          {project.id % 2 === 0 ? (
+            <>
+              <Col md={6} className="mb-4 h-100">
+                <Card className="in-view ">
+                  <Card.Img
+                    variant="top"
+                    src={project.imgSrc}
+                    alt={project.imgAlt}
+                  />
+                </Card>
+              </Col>
 
-                <div className="d-flex flex-row justify-content-between mt-auto">
-                  <Link
-                    to={project.projectLink}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="mt-auto mx-auto btn btn-primary"
-                    style={{ width: "150px" }}
-                  >
-                    View Code
-                  </Link>
-                  <Link
-                    to={project.ProjectLive}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="mt-auto mx-auto btn btn-primary"
-                    style={{ width: "150px" }}
-                  >
-                    View Live
-                  </Link>
-                </div>
-              </Card.Body>
-            </Card>
-          </Col>
-        ))}
-      </Row>
+              <Col md={6} className="mb-4">
+                <Card className="in-view h-100">
+                  <Card.Body className="d-flex flex-column">
+                    <Card.Title>{project.title}</Card.Title>
+
+                    {Array.isArray(project.text) ? (
+                      project.text.map((line, index) => (
+                        <Card.Text key={index}>{line}</Card.Text>
+                      ))
+                    ) : (
+                      <Card.Text>{project.text}</Card.Text>
+                    )}
+
+                    <div className="d-flex flex-row justify-content-between mt-auto">
+                      <Link
+                        to={project.projectLink}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="mt-auto mx-auto   project-btn">
+                        View Code
+                      </Link>
+                      <Link
+                        to={project.ProjectLive}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="mt-auto mx-auto  project-btn">
+                        View Live
+                      </Link>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </>
+          ) : (
+            <>
+              <Col md={6} className="mb-4">
+                <Card className="in-view h-100">
+                  <Card.Body className="d-flex flex-column">
+                    <Card.Title>{project.title}</Card.Title>
+
+                    {Array.isArray(project.text) ? (
+                      project.text.map((line, index) => (
+                        <Card.Text key={index}>{line}</Card.Text>
+                      ))
+                    ) : (
+                      <Card.Text>{project.text}</Card.Text>
+                    )}
+
+                    <div className="d-flex flex-row justify-content-between mt-auto">
+                      <Link
+                        to={project.projectLink}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="mt-auto mx-auto   project-btn">
+                        View Code
+                      </Link>
+                      <Link
+                        to={project.ProjectLive}
+                        target="_blank"
+                        rel="noreferrer"
+                        className="mt-auto mx-auto  project-btn">
+                        View Live
+                      </Link>
+                    </div>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col md={6} className="mb-4 h-100">
+                <Card className="in-view ">
+                  <Card.Img
+                    variant="top"
+                    src={project.imgSrc}
+                    alt={project.imgAlt}
+                  />
+                </Card>
+              </Col>
+            </>
+          )}
+        </Row>
+      ))}
     </Container>
   );
 }
